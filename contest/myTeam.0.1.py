ea5f34d6963ecbf0258f99de879ec646d6da212c
rmit-s3323595-yohanes-keanoe <s3323595@student.rmit.edu.au>
2017-06-04 23:19:06 +1000

    Moved final team to my team and clean up code

--- contest/myTeam.0.1.py
+++ contest/myTeam.0.1.py
@@ -22,34 +22,30 @@ import game
 #################
 
 def createTeam(firstIndex, secondIndex, isRed,
-			   first = 'OffensiveAgent', second = 'DefensiveAgent'):
+               first = 'OffensiveAgent', second = 'DefensiveAgent'):
 	return [eval(first)(firstIndex), eval(second)(secondIndex)]
 
 ##########
 # Agents #
 ##########
 
-class AlphaBetaAgent(CaptureAgent):
+class ExpectimaxAgent(CaptureAgent):
 
-	# Register initial state for AlphaBetaAgent include start position of the
-	# agent, closest food position to the center and timer for defensive agent,
-	# and mid point for center position.
 	def registerInitialState(self, gameState):
 		CaptureAgent.registerInitialState(self, gameState)
 		self.start = gameState.getAgentPosition(self.index)
-		self.closestFoodToCenter = None
+
+		self.furthestFood = None
 		self.timer = 0
 
-		# Setting up center position and check if the center position has a wall.
 		midHeight = gameState.data.layout.height/2
 		midWidth = gameState.data.layout.width/2
 		while(gameState.hasWall(midWidth, midHeight)):
 			midHeight -= 1
-			if midHeight == 0:
-				midHeight = gameState.data.layout.height
-		self.midPoint = (midWidth, midHeight)
 
-		# Register team's agent.
+		self.midPoint = ( midWidth, midHeight )
+
+
 		agentsTeam = []
 		agentsLen = gameState.getNumAgents()
 		i = self.index
@@ -61,31 +57,71 @@ class AlphaBetaAgent(CaptureAgent):
 		agentsTeam.sort()
 		self.registerTeam(agentsTeam)
 
-	# Alphabeta algorithm with the usual algorithm added with visible agents.
-	# If the agent are not in visible agents, it will continue the loop to the
-	# next agent.
+	def baseSearch(self, gameState):
+		"""
+        Picks among the actions with the highest Q(s,a).
+        """
+		actions = gameState.getLegalActions(self.index)
+
+		# You can profile your evaluation time by uncommenting these lines
+		# start = time.time()
+		v = []
+		for action in actions:
+			if action == 'Stop':
+				continue
+			successor = gameState.generateSuccessor(self.index, action)
+			v.append((self.evaluate(successor), action))
+
+		# print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
+
+		# print "Values in baseSearch ", v
+		maxValues = [i for i in v if i == max(v)]
+		maxValue = random.choice(maxValues)
+
+		# print maxValue
+		bestAction = maxValue[1]
+		# print bestAction
+		foodLeft = len(self.getFood(gameState).asList())
+		# print foodLeft
+		if foodLeft <= 2:
+			# print "foodl <= 2"
+			bestDist = 9999
+			for action in actions:
+				if action == 'Stop':
+					continue
+				successor = gameState.generateSuccessor(self.index, action)
+				pos2 = successor.getAgentPosition(self.index)
+				dist = self.getMazeDistance(gameState.getAgentPosition(self.index), pos2)
+				# print "Comparing {0} < {1}".format(dist, bestDist)
+				if dist < bestDist:
+					# print "Change action to ",action
+					bestAction = action
+					bestDist = dist
+			# print "Base search return with ", bestAction
+			return bestAction
+		# print "Base search return with ",bestAction
+		return bestAction
+
+
 	def alphabeta(self, gameState, agent, mdepth, alpha, beta, visibleAgents):
 
-		# restart the agent number if it passed the agents length
 		if agent >= gameState.getNumAgents():
 			agent = 0
-
-		# add the depth if the alpha beta done a single loop
 		if agent == self.index:
 			mdepth += 1
 
-		# pass the agent if it is not on the visible agents.
 		if agent not in visibleAgents:
 			return self.alphabeta(gameState, agent + 1, mdepth, alpha, beta, visibleAgents)
 
-		# evaluate the gameState if the depth is 1 or the game is over and its the current agent.
-		if mdepth == 1 or gameState.isOver():
-			if agent == self.index:
-				return self.evaluate(gameState)
-			else:
-				self.alphabeta(gameState, agent + 1, mdepth, alpha, beta, visibleAgents)
-
 		legalActions = gameState.getLegalActions(agent)
+
+		if (mdepth == 1 or gameState.isOver()) and agent == self.index:
+			return self.evaluate(gameState)
+		else:
+			self.alphabeta(gameState, agent + 1, mdepth, alpha, beta, visibleAgents)
+
+		# tempActions = list(set(legalActions).difference(['Stop']))
+
 		if agent in self.agentsOnTeam:
 			v = float("-inf")
 			for action in legalActions:
@@ -103,29 +139,39 @@ class AlphaBetaAgent(CaptureAgent):
 				beta = min(beta, v)
 			return v
 
-	# Old expectiminimax algorithm that is not used.
 	def expectiminimax(self, gameState, agent, mdepth, visibleAgents):
+		# print "start expectimax with agent {0} at depth {1}".format(agent, mdepth)
 
 		if agent >= gameState.getNumAgents():
+			# print "agent rollback to 0"
 			agent = 0
 			mdepth += 1
+			# print "continue expectimax with agent {0} at depth {1}".format(agent, mdepth)
+
 
 		# Depth 3 or more takes longer than 3 second
 		if mdepth == 2 or gameState.isOver():
+			# print "stopping"
 			e = self.evaluate(gameState)
+			# print "evaluate = ",e
 			return e
 
 		if agent not in visibleAgents:
+			# print "agent {0} not visible".format(agent)
 			return self.expectiminimax(gameState, agent + 1, mdepth, visibleAgents)
 
 		else:
+			# print "Getting legalActions for agent {0}".format(agent)
 			legalActions = gameState.getLegalActions(agent)
 			if agent in self.agentsOnTeam:
-				v = float("-inf")
+				# v = float("-inf")
+				v = []
 				for action in legalActions:
 					if action == 'Stop':
 						continue
-					v = max(v, self.expectiminimax(gameState.generateSuccessor(agent, action), agent + 1,mdepth, visibleAgents))
+					v.append(self.expectiminimax(gameState.generateSuccessor(agent, action), agent + 1,mdepth, visibleAgents))
+					# v = max(v, self.expectiminimax(gameState.generateSuccessor(agent, action), agent + 1,mdepth, visibleAgents))
+				# print v
 				return max(v)
 			else:
 				v = []
@@ -133,27 +179,8 @@ class AlphaBetaAgent(CaptureAgent):
 					expectVals = self.expectiminimax(gameState.generateSuccessor(agent, action), agent + 1,mdepth, visibleAgents)
 					v.append(expectVals)
 				ret = sum(v)/len(legalActions)
+				# print ret
 				return ret
-
-	# Used to check if the current path taken will result in dead end.
-	# Not finish yet so it may not work as intended.
-	def iterativeDeep(self, gameState, action, visibleAgents):
-		successor = gameState.generateSuccessor(self.index, action)
-		depth = 0
-		while depth < 5:
-			legalActions = successor.getLegalActions(self.index)
-			invalidActions = [action, 'Stop']
-			validActions = list(set(legalActions).difference(set(invalidActions)))
-			if len(validActions) == 0:
-				return 0
-			if len(validActions) > 1:
-				return 1
-			successor = gameState.generateSuccessor(self.index, action)
-			depth += 1
-		if set(visibleAgents).isdisjoint(self.getOpponents(gameState)):
-			return 1
-		else:
-			return 0
 
 	def getFeatures(self, gameState):
 		"""
@@ -180,8 +207,8 @@ class AlphaBetaAgent(CaptureAgent):
 		weights = self.getWeights(gameState)
 		return features * weights
 
-class OffensiveAgent(AlphaBetaAgent):
-	# The basic of offensive agent is to grab all the food while avoiding ghost.
+class OffensiveAgent(ExpectimaxAgent):
+
 	def chooseAction(self, gameState):
 		start = time.time()
 
@@ -189,7 +216,6 @@ class OffensiveAgent(AlphaBetaAgent):
 		allAgents = range(0, gameState.getNumAgents())
 		visibleAgents = [a for a in allAgents if gameState.getAgentState(a).getPosition() is not None]
 
-		# Start alpha beta pruning algorithm
 		v = (float("-inf"), 'None')
 		alpha = float('-inf')
 		beta = float('inf')
@@ -210,52 +236,53 @@ class OffensiveAgent(AlphaBetaAgent):
 	def getFeatures(self, gameState):
 		features = util.Counter()
 		foodList = self.getFood(gameState).asList()
-		# Check if agent is red or blue. red team will want a higher score
-		# while blue team want a lower score
 		if self.red:
-			features['successorScore'] = gameState.getScore()
+			features['successorScore'] = gameState.getScore()  # self.getScore(successor)
 		else:
-			features['successorScore'] = -1* gameState.getScore()
-		features['successorScore'] -= len(foodList)
+			features['successorScore'] = -1* gameState.getScore()  # self.getScore(successor)
+		features['successorScore'] -= len(foodList)  # self.getScore(successor)
 		features['distanceToGhost'] = 0
 
-		# Get all visible agents
+		# 	# Get all visible agents
 		allAgents = range(0, gameState.getNumAgents())
 		visibleAgents = [a for a in allAgents if gameState.getAgentState(a).getPosition() != None]
 
 		currPos = gameState.getAgentState(self.index).getPosition()
 
-		# Check if any opponents in visible agents.
+
 		if not set(visibleAgents).isdisjoint(self.getOpponents(gameState)):
-			# Agent will need to distance themself from ghost if agent is a pacman or agent is scared.
+			# print "ghost visible"
 			if gameState.getAgentState(self.index).isPacman and gameState.getAgentState(self.index).scaredTimer > 0:
+				# print "ghost visible, we pacman, and ghost aint scared"
 				ghosts = list(set(visibleAgents).intersection(self.getOpponents(gameState)))
 				for ghost in ghosts:
 					ghostPos = gameState.getAgentState(ghost).getPosition()
 					dist = self.getMazeDistance(currPos, ghostPos)
-					# Agent will never move to less than 2 distance.
+					# print "distance to ghost ", dist
 					if dist <= 2:
 						features['distanceToGhost'] = -9999
 					else:
 						features['distanceToGhost'] += 0.5 * dist
-			# Agent does not need to further itself from the ghost but it is better to avoid/hide from it.
 			else:
+			# 	print "ghost visible but we aint pacman or ghosts scared"
 				ghosts = list(set(visibleAgents).intersection(self.getOpponents(gameState)))
 				for ghost in ghosts:
 					ghostPos = gameState.getAgentState(ghost).getPosition()
 					dist = self.getMazeDistance(currPos, ghostPos)
 					# print "distance to ghost ", dist
 					features['distanceToGhost'] += 0.5 * dist
-		# If no opponent are visible, agent will just try to read the noise distance and try to
-		# stay away from it.
 		else:
+			# print "ghost aint visible"
 			ghosts = list(set(allAgents).difference(self.agentsOnTeam))
 			for ghost in ghosts:
 				ghostDists = gameState.getAgentDistances()
+				# print "distance to ghost ", ghostDists[ghost]
 				features['distanceToGhost'] += ghostDists[ghost]
 
-		# Agent will grab the nearest food unless it's already carrying more than 5 food.
-		if gameState.getAgentState(self.index).numCarrying < 5:
+		# Compute distance to the nearest food
+		# myPos = gameState.getAgentState(self.index).getPosition()
+		# print self.getMazeDistance(myPos, self.start)
+		if gameState.getAgentState(self.index).numCarrying < 5:  # This should always be True,  but better safe than sorry
 			myPos = gameState.getAgentState(self.index).getPosition()
 			if len(foodList) > 0:
 				minDistance = min([self.getMazeDistance(myPos, food) for food in foodList])
@@ -264,24 +291,24 @@ class OffensiveAgent(AlphaBetaAgent):
 				myPos = gameState.getAgentState(self.index).getPosition()
 				features['distanceToFood'] = self.getMazeDistance(myPos, self.start)
 		else:
+			# print "bring food home"
 			myPos = gameState.getAgentState(self.index).getPosition()
 			features['distanceToFood'] = self.getMazeDistance(myPos, self.start)
+			# print features
 		return features
 
 	def getWeights(self, gameState):
 		return {'successorScore': 100, 'distanceToFood': -1, 'distanceToGhost': 1}
 
-class DefensiveAgent(AlphaBetaAgent):
-	# The basic of defensive agent is to go around the nearest food to the center to detect an enemy
-	# and chase one if found.
+class DefensiveAgent(ExpectimaxAgent):
 	def chooseAction(self, gameState):
 		start = time.time()
+
 
 		# Get all visible agents
 		allAgents = range(0, gameState.getNumAgents())
 		visibleAgents = [a for a in allAgents if gameState.getAgentState(a).getPosition() is not None]
 
-		# Start alpha beta algorithm
 		v = (float("-inf"), 'None')
 		alpha = float('-inf')
 		beta = float('inf')
@@ -297,48 +324,59 @@ class DefensiveAgent(AlphaBetaAgent):
 			alpha = max(alpha, v[0])
 		print "Agent {0} chose {1} with value {2}".format(self.index, v[1], v[0])
 		print 'Execution time for agent %d: %.4f' % (self.index, time.time() - start)
-
-		# Minus the timer for the patrol function.
 		self.timer -= 1
-
+		print self.furthestFood
 		return v[1]
+
 
 	def getFeatures(self, gameState):
 		features = util.Counter()
+
 		myState = gameState.getAgentState(self.index)
 		myPos = myState.getPosition()
-		foodList = self.getFoodYouAreDefending(gameState).asList()
+
+		# Computes whether we're on defense (1) or offense (0)
+		# features['onDefense'] = 1
+		# if myState.isPacman: features['onDefense'] = 0
 
 		# Computes distance to invaders we can see
 		enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
 		invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
 		features['numInvaders'] = len(invaders)
 
-		# Check if any opponent is found.
+		foodList = self.getFoodYouAreDefending(gameState).asList()
+
 		if len(invaders) > 0:
 			dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
 			features['invaderDistance'] = max(dists)
 		else:
-			# If no opponent is found, patrol around the 3 nearest food to the center.
-			# if the nearest food to center is set, calculate the distance.
-			if self.closestFoodToCenter:
-				dist = self.getMazeDistance(myPos, self.closestFoodToCenter)
+
+			if self.furthestFood:
+				dist = self.getMazeDistance(myPos, self.furthestFood)
 			else:
 				dist = None
 
-			# Recalculate the 3 nearest food when it's already 20 actions or the food
-			# is reached.
 			if self.timer == 0 or dist == 0:
 				self.timer = 20
+
 				foods = []
 				for food in foodList:
 					foods.append((self.getMazeDistance(self.midPoint, food), food))
 				foods.sort()
+				print foods
+				print foods[:3]
 				chosenFood = random.choice(foods[:3])
-				self.closestFoodToCenter = chosenFood[1]
-			dist = self.getMazeDistance(myPos, self.closestFoodToCenter)
+				# print chosenFood
+				self.furthestFood = chosenFood[1]
+
+			dist = self.getMazeDistance(myPos, self.furthestFood)
+
 			features['invaderDistance'] = dist
+
+			# dists = [self.getMazeDistance(myPos, a) for a in foodList]
+			# features['invaderDistance'] = random.choice(dists)
+
 		return features
 
 	def getWeights(self, gameState):
-		return {'numInvaders': -1000, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
+		return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
--- contest/yohanTeam.py
+++ contest/yohanTeam.py
@@ -63,7 +63,7 @@ class ExpectimaxAgent(CaptureAgent):
 
 		# print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
 
-		print "Values in baseSearch ", v
+		# print "Values in baseSearch ", v
 		maxValues = [i for i in v if i == max(v)]
 		maxValue = random.choice(maxValues)
 
@@ -86,9 +86,9 @@ class ExpectimaxAgent(CaptureAgent):
 					# print "Change action to ",action
 					bestAction = action
 					bestDist = dist
-			print "Base search return with ", bestAction
+			# print "Base search return with ", bestAction
 			return bestAction
-		print "Base search return with ",bestAction
+		# print "Base search return with ",bestAction
 		return bestAction
 
 
@@ -102,8 +102,8 @@ class ExpectimaxAgent(CaptureAgent):
 			# print "continue expectimax with agent {0} at depth {1}".format(agent, mdepth)
 
 
-
-		if mdepth == 1 or gameState.isOver():
+		# Depth 3 or more takes longer than 3 second
+		if mdepth == 2 or gameState.isOver():
 			# print "stopping"
 			e = self.evaluate(gameState)
 			# print "evaluate = ",e
@@ -158,28 +158,21 @@ class ExpectimaxAgent(CaptureAgent):
 		"""
 		features = self.getFeatures(gameState)
 		weights = self.getWeights(gameState)
+		# print features
+		# print weights
 		return features * weights
 
 class OffensiveAgent(ExpectimaxAgent):
 
 	def chooseAction(self, gameState):
-
+		start = time.time()
 		# Get all visible agents
 		allAgents = range(0, gameState.getNumAgents() - 1)
 		visibleAgents = [a for a in allAgents if gameState.getAgentState(a).getPosition() is not None]
-		print visibleAgents
-
-		print gameState.getAgentState(1).getPosition()
-		print gameState.getAgentState(3).getPosition()
-		# print gameState.getAgentState(self.index).isPacman
-
-		ourAgent = self.getOpponents(gameState)
-		# print ourAgent
-
 
 
 		# if not set(visibleAgents).isdisjoint(self.getOpponents(gameState)):
-		print "Starting expetiminimax"
+		# print "Starting expetiminimax"
 		v = []
 		agent = self.index
 		legalActions = gameState.getLegalActions(self.index)
@@ -189,11 +182,13 @@ class OffensiveAgent(ExpectimaxAgent):
 				continue
 			expectVals = self.expectiminimax(gameState.generateSuccessor(agent,action), agent+1, 0, visibleAgents)
 			v.append((expectVals, action))
-		print "Expectiminimax val = ", v
+		# print "Expectiminimax val = ", v
 		vBestScore = max(v)
 		bestIndices = [index for index in range(len(v)) if v[index] == vBestScore]
 		chosenIndex = random.choice(bestIndices) # Pick randomly among the best
-		print "Expectiminimax return with ", v[chosenIndex][1]
+		# print "Expectiminimax return with ", v[chosenIndex][1]
+		# print 'eval time for agent %d: %.4f' % (self.index, time.time() - start)
+
 		return v[chosenIndex][1]
 		# return random.choice(legalActions)
 		# else:
@@ -236,29 +231,7 @@ class OffensiveAgent(ExpectimaxAgent):
 	def getWeights(self, gameState):
 		return {'successorScore': 100, 'distanceToFood': -1}
 
-	# def evaluate(self, gameState):
-	# 	"""
-     #    Computes a linear combination of features and feature weights
-     #    """
-	# 	features = self.getFeatures(gameState)
-	# 	print features
-	# 	weights = self.getWeights(gameState)
-	# 	print weights
-	# 	print features * weights
-	# 	print type(features)
-	# 	print type(weights)
-	# 	return features * weights
-
 	def evaluate(self, gameState):
-		"""
-		Score = 1 * currentScore
-				- 1 * distance to closest food
-				- 2 * distance to closest non scared ghost
-				- 20 * distance to closest capsule
-		"""
-		# print "eva"
-
-
 		# Get all visible agents
 		allAgents = range(0, gameState.getNumAgents() - 1)
 		visibleAgents = [a for a in allAgents if gameState.getAgentState(a).getPosition() != None]
@@ -266,58 +239,53 @@ class OffensiveAgent(ExpectimaxAgent):
 		score = gameState.getScore()
 		currPos = gameState.getAgentState(self.index).getPosition()
 
-		# print "CurrPos {0}, self.start {1}".format(currPos, self.start)
-		# if currPos == self.start:
-		# 	print "Dying"
-		# 	return float('-inf')
-
+		# If no enemies is visible
 		if not set(visibleAgents).isdisjoint(self.getOpponents(gameState)):
-			ghosts = list(set(visibleAgents).intersection(self.getOpponents(gameState)))
-			# print ghosts
-			for ghostIndex in ghosts:
-				ghost = gameState.getAgentState(ghostIndex)
-				if ghost.isPacman:
-					continue
-				ghostPos = ghost.getPosition()
-				if ghost.scaredTimer == 0:
-					dist = self.getMazeDistance(currPos, ghostPos)
-					# print "distance to ghost ", dist
-					if dist <= 2:
-						return float('-inf')
-
+			# If offensive agent is pacman
+			if gameState.getAgentState(self.index).isPacman:
+				ghosts = list(set(visibleAgents).intersection(self.getOpponents(gameState)))
+				for ghostIndex in ghosts:
+					ghost = gameState.getAgentState(ghostIndex)
+					if ghost.isPacman:
+						continue
+					ghostPos = ghost.getPosition()
+					if ghost.scaredTimer == 0:
+						dist = self.getMazeDistance(currPos, ghostPos)
+						if dist <= 2:
+							return float('-inf')
+						else:
+							score += 20 * dist
 
 		capsules = self.getCapsules(gameState)
 		foodList = self.getFood(gameState).asList()
 
+		# Try to get capsule if carrying more than 5 food
+		if gameState.getAgentState(self.index).numCarrying > 5 and capsules:
+			score -= 10 * min([self.getMazeDistance(currPos, capsule) for capsule in capsules])
+
 		# Return home when half of the food is eaten
-		# if gameState.getAgentState(self.index).numCarrying >= len(foodList):
-		# 	score -= 1000 * self.getMazeDistance(currPos, self.start)
+		# if gameState.getAgentState(self.index).numCarrying >= 5:
+		# 	print "going home"
+		# 	score -= 200 * self.getMazeDistance(currPos, self.start)
+		# 	print score
 		# else:
 		score -= 100 * len(foodList)
-		# score -= 100 * len(capsules)
-
-		score -= min([self.getMazeDistance(currPos, food) for food in foodList])
+		if len(foodList) > 0:
+			score -= min([self.getMazeDistance(currPos, food) for food in foodList])
 			# score -= min([self.getMazeDistance(currPos, capsule) for capsule in capsules])
-
 			# score += 2 * gameState.getAgentState(self.index).numCarrying
-
-
 			# for food in foodList:
 			# 	score -= 1.5 * self.getMazeDistance(currPos, food)
-			#
-			#
 			# for capsule in capsules:
 			# 	score -= 2 * self.getMazeDistance(currPos, capsule)
-
-			# print "eva return with score ", score
 		return score
 
 
 class DefensiveAgent(ExpectimaxAgent):
 	def chooseAction(self, gameState):
-		legalActions = gameState.getLegalActions(self.index)
-		return random.choice(legalActions)
-		# return self.baseSearch(gameState)
+		# legalActions = gameState.getLegalActions(self.index)
+		# return random.choice(legalActions)
+		return self.baseSearch(gameState)
 
 	def getFeatures(self, gameState):
 		features = util.Counter()
@@ -333,12 +301,20 @@ class DefensiveAgent(ExpectimaxAgent):
 		enemies = [gameState.getAgentState(i) for i in self.getOpponents(gameState)]
 		invaders = [a for a in enemies if a.isPacman and a.getPosition() != None]
 		features['numInvaders'] = len(invaders)
+
+		foodList = self.getFoodYouAreDefending(gameState).asList()
+
 		if len(invaders) > 0:
 			dists = [self.getMazeDistance(myPos, a.getPosition()) for a in invaders]
 			features['invaderDistance'] = min(dists)
+		else:
+			dists = [self.getMazeDistance(myPos, a) for a in foodList]
+			features['invaderDistance'] = random.choice(dists)
 
 		return features
 
+	def getWeights(self, gameState):
+		return {'numInvaders': -1000, 'onDefense': 100, 'invaderDistance': -10, 'stop': -100, 'reverse': -2}
 
 
 
